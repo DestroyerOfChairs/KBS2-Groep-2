b25673aec07f29754ce85b018d89f114229b8769
JorrinKievit <43169049+JorrinKievit@users.noreply.github.com>
2019-04-25 13:56:11 +0200

    setup applicatiefiles

---
+++ Applicatie/Applicatie/ApplicatieScherm.java
@@ -0,0 +1,17 @@
+package Applicatie;
+
+import null.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jorrin
+ */
+public class ApplicatieScherm {
+    
+}
